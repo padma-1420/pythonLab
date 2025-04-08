88227809afc10874b4bdb5b668e2e081115bd5b3
padma-1420 <shekarpriya29@gmail.com>
2025-04-08 18:44:09 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,6 @@
+def count_char_occurrences(s, char):
+    return s.count(char)
+s = "hello"
+char = 'l'
+result = count_char_occurrences(s, char)
+print(result) 
---
+++ remove.py
@@ -0,0 +1,6 @@
+def remove_char(s, char):
+    return s.replace(char, '')
+s = "hello"
+char = 'l'
+result = remove_char(s, char)
+print(result)  
